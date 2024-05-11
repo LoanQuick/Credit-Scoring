e4b8c61d1fa54a753d9a6a5b3d40114014b72e2c
SREYANSH BARANWAL <sreyanshbaranwal@gmail.com>
2024-05-12 05:03:38 +0530

    all APIs integrated

--- FINAL CREDIT SCORING AND PREDICTING/Final1.py
+++ FINAL CREDIT SCORING AND PREDICTING/Final1.py
@@ -9,6 +9,8 @@ from sklearn.ensemble import RandomForestRegressor
 import joblib
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LinearRegression
+from dateutil.relativedelta import relativedelta
 
 data1 = pd.read_csv("train_predict_credit.csv")
 data2= pd.read_csv("train_predict_score.csv")
@@ -149,6 +151,47 @@ def predict_score():
 
     # Return the result as JSON
     return jsonify(result)
+
+
+@app.route('/predict_month_revenue_expenses', methods=['POST'])
+def predict_month_revenue_expenses():
+    # Load the input data from a JSON request
+    data_dict = request.get_json()
+    data = pd.DataFrame(data_dict)
+    data['date'] = pd.to_datetime(data['date'])
+
+    # Convert 'date' column to datetime and set as index
+    data.set_index('date', inplace=True)
+
+    # Aggregate to monthly data
+    monthly_data = data.resample('M').sum()
+
+    # Create 'month_index' column
+    monthly_data['month_index'] = range(1, len(monthly_data) + 1)
+
+    # Train the models
+    model_revenue = LinearRegression().fit(monthly_data[['month_index']], monthly_data['daily_revenue'])
+    model_expenses = LinearRegression().fit(monthly_data[['month_index']], monthly_data['daily_expenses'])
+
+    # Predict for the next two months
+    last_month_index = monthly_data['month_index'].max()
+    predicted_revenues = model_revenue.predict([[last_month_index + 1], [last_month_index + 2]])
+    predicted_expenses = model_expenses.predict([[last_month_index + 1], [last_month_index + 2]])
+
+    # Get the last date in the data
+    last_date = monthly_data.index.max()
+
+    # Create a dictionary with the predicted revenues and expenses
+    result = {
+        (last_date + relativedelta(months=i)).strftime('%Y-%m'): {
+            'predicted_revenue': predicted_revenues[i-1],
+            'predicted_expenses': predicted_expenses[i-1]
+        } for i in range(1, 3)
+    }
+
+    # Return the result as JSON
+    return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
